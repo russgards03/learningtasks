fb628df62b0dfb702d51518668a76b837d6765bf
russgards03 <99627318+russgards03@users.noreply.github.com>
2022-02-20 13:29:32 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Homepage </title>
+  </head>
+  <body>
+  <h1> Welcome to my website! I am Russ! </h1>
+  <ul>
+    <li> <a href= "index.php"> HOME </a> </li>
+    <li> <a href= "/LT2/index.html"> LEARNING TASK 2 </a> </li>
+  </ul>
+  </body>
+  </html>

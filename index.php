4bd718a34b5c73d6ed106ddf75953e498c057047
russgards03 <99627318+russgards03@users.noreply.github.com>
2022-04-19 09:24:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <li> <a href= "/LT3/index.html"> LEARNING TASK 3 </a> </li>
     <li> <a href= "/CLE1MT/index.html"> CULMINATING LEARNING EVIDENCE 1 </a> </li>
     <li> <a href= "/LT4/index.html"> LEARNING TASK 4 </a> </li>
+    <li> <a href= "/LT5/index.html"> LEARNING TASK 5 </a> </li>
   </ul>
   </body>
   </html>

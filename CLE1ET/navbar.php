4871f343385c11e0af30867801a2dde185497364
russgards03 <99627318+russgards03@users.noreply.github.com>
2022-05-23 10:19:42 +0800

    Update navbar.php

--- CLE1ET/navbar.php
+++ CLE1ET/navbar.php
@@ -5,9 +5,8 @@ $php = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
 <html>
     <link rel= "stylesheet" href= "css/stylenav.css">
     <ul class ="navigation">
-        <li><a href = index.php> Home </a></li>
-        <li><a href = profile.php> Creator's Profile </a></li>
-        <li><a href = works.php> Creator's Works </a></li>
-        <li><a href = gallery.php> Gallery </a></li>
+        <li><a href = "index.php?page=home"> Home </a></li>
+        <li><a href = "profile.php?page=profile"> Creator's Profile </a></li>
+        <li><a href = "works.php?page=works"> Creator's Works </a></li>
+        <li><a href = "gallery.php?page=gallery"> Gallery </a></li>
     </ul>
-        
